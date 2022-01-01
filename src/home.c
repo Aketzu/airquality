6dd41a5ba7c2e3204669cf5bdd424d505cfb37e1
Anssi Kolehmainen <anssi@aketzu.net>
2022-01-01 13:43:44 +0200

    Rewrite i2c handling

--- src/home.c
+++ src/home.c
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 
 #include "core2forAWS.h"
 
@@ -21,102 +22,132 @@ uint8_t SensCrc(uint8_t data[2]) {
 #undef PORT_A_I2C_STANDARD_BAUD
 #define PORT_A_I2C_STANDARD_BAUD 50000
 
+struct sensor {
+  char name[20];
+  I2CDevice_t dev;
+};
+
+struct sensor* init_sensor(int i2c_addr, char *name) {
+  struct sensor *sens = calloc(1, sizeof(struct sensor));
+  sens->dev = Core2ForAWS_Port_A_I2C_Begin(i2c_addr, 50000);
+
+  strncpy(sens->name, name, sizeof(sens->name));
+
+  return sens;
+}
+
+void free_sensor(struct sensor *sens) {
+  Core2ForAWS_Port_A_I2C_Close(sens->dev);
+  free(sens);
+}
+
+esp_err_t sensor_send(struct sensor *sens, void *data, int data_len, int wait, void *resp, int resp_len) {
+  esp_err_t err;
+
+  if ((data_len > 0 && !data) || (resp_len > 0 && !resp)) {
+    return ESP_ERR_INVALID_ARG;
+  }
+
+  err = Core2ForAWS_Port_A_I2C_Write(sens->dev, I2C_NO_REG, data, data_len);
+  if (err) {
+    ESP_LOGI(sens->name, "Write failed: %s", esp_err_to_name(err));
+    return err;
+  }
+  if (wait > 0) {
+    vTaskDelay(pdMS_TO_TICKS(wait));
+  }
+
+  if (resp_len < 1) {
+    return ESP_OK;
+  }
+
+  uint8_t *retbuf = calloc(1, resp_len);
+  err = Core2ForAWS_Port_A_I2C_Read(sens->dev, I2C_NO_REG, retbuf, resp_len);
+  if (err) {
+    ESP_LOGI(sens->name, "Read failed: %s", esp_err_to_name(err));
+    return err;
+  }
+
+  // If Sensirion do CRC checks
+  if (sens->name[0] == 'S') { // Works for me
+    uint8_t *rp = resp;
+    for (int i=0; i<resp_len; i+=3) {
+      if (SensCrc(&retbuf[i]) != retbuf[i+2]) {
+        ESP_LOGI(sens->name, "CRC error pos %d: 0x%02x != CRC(0x%02x, 0x%02x)", i, retbuf[i+2], retbuf[i], retbuf[i+1]);
+        free(retbuf);
+        return ESP_ERR_INVALID_CRC;
+      }
+      *rp++ = retbuf[i];
+      *rp++ = retbuf[i+1];
+    }
+  } else {
+    memcpy(resp, retbuf, resp_len);
+  }
+
+  free(retbuf);
+  return ESP_OK;
+}
+
+
 void i2c_task(void *par) {
   lv_obj_t *label = (lv_obj_t*)par;
   esp_err_t err;
 
-
-  I2CDevice_t sht40 = Core2ForAWS_Port_A_I2C_Begin(0x44, PORT_A_I2C_STANDARD_BAUD);
-
+  struct sensor *sht40 = init_sensor(0x44, "SHT40");
   // Reset
-  err = Core2ForAWS_Port_A_I2C_Write(sht40, I2C_NO_REG, (uint8_t*) "\x94", 1);
-  vTaskDelay(pdMS_TO_TICKS(1));
+  sensor_send(sht40, "\x94", 1, 10, 0, 0);
 
   // Read serial
-  err = Core2ForAWS_Port_A_I2C_Write(sht40, I2C_NO_REG, (uint8_t*) "\x89", 1);
-  vTaskDelay(pdMS_TO_TICKS(1));
-
-  uint8_t shtser[6] = {};
-  err = Core2ForAWS_Port_A_I2C_Read(sht40, I2C_NO_REG, shtser, sizeof(shtser));
-  if(!err){
-    if (SensCrc(&shtser[0]) != shtser[2] || SensCrc(&shtser[3]) != shtser[5]) {
-      ESP_LOGI("SHT40", "CRC error in serial");
-    }
-    ESP_LOGI("SHT40", "Sensor serial %02x %02x %02x %02x", shtser[0], shtser[1], shtser[3], shtser[4]);
+  uint8_t shtser[4] = {};
+  err = sensor_send(sht40, "\x89", 1, 1, shtser, 6);
+  if (!err) {
+    ESP_LOGI("SHT40", "Sensor serial %02x %02x %02x %02x", shtser[0], shtser[1], shtser[2], shtser[3]);
   }
 
-  vTaskDelay(pdMS_TO_TICKS(50));
-
-
-  /*
-  I2CDevice_t sgp40 = Core2ForAWS_Port_A_I2C_Begin(0x59, PORT_A_I2C_STANDARD_BAUD);
-
+  struct sensor *sgp40 = init_sensor(0x59, "SGP40");
   // Reset
-  err = Core2ForAWS_Port_A_I2C_Write(sgp40, I2C_NO_REG, (uint8_t*) "\x00\x06", 2);
-  vTaskDelay(pdMS_TO_TICKS(1));
+  sensor_send(sgp40, "\x00\x06", 2, 1, 0, 0);
 
   // Read serial
-  err = Core2ForAWS_Port_A_I2C_Write(sgp40, I2C_NO_REG, (uint8_t*) "\x36\x82", 2);
-  vTaskDelay(pdMS_TO_TICKS(1));
-  uint8_t sgpser[9] = {};
-  err = Core2ForAWS_Port_A_I2C_Read(sgp40, I2C_NO_REG, sgpser, sizeof(sgpser));
+  uint8_t sgpser[6] = {};
+  err = sensor_send(sgp40, "\x36\x82", 1, 1, sgpser, 9);
   if(!err){
-    if (SensCrc(&sgpser[0]) != sgpser[2] || SensCrc(&sgpser[3]) != sgpser[5] || SensCrc(&sgpser[6]) != sgpser[8]) {
-      ESP_LOGI("SGP40", "CRC error in serial");
-    }
-    ESP_LOGI("SGP40", "Sensor serial %02x %02x %02x %02x %02x %02x", sgpser[0], sgpser[1], sgpser[3], sgpser[4], sgpser[6], sgpser[7]);
+    ESP_LOGI("SGP40", "Sensor serial %02x %02x %02x %02x %02x %02x", sgpser[0], sgpser[1], sgpser[2], sgpser[3], sgpser[4], sgpser[5]);
   }
-  */
 
+  struct sensor *sps30 = init_sensor(0x69, "SPS30");
+  // Wakup
+  sensor_send(sps30, "\x11\x03", 2, 5, 0, 0);
 
-  I2CDevice_t sps30 = Core2ForAWS_Port_A_I2C_Begin(0x69, PORT_A_I2C_STANDARD_BAUD);
-  // Wakeup
-  err = Core2ForAWS_Port_A_I2C_Write(sps30, I2C_NO_REG, (uint8_t*) "\x11\x03", 2);
-  vTaskDelay(pdMS_TO_TICKS(5));
-
-  // Get serial
-  err = Core2ForAWS_Port_A_I2C_Write(sps30, I2C_NO_REG, (uint8_t*) "\xd0\x33", 2);
-  vTaskDelay(pdMS_TO_TICKS(1));
-  uint8_t spsser[48] = {};
-  err = Core2ForAWS_Port_A_I2C_Read(sps30, I2C_NO_REG, spsser, sizeof(spsser));
-  if(!err){
-    char serial[32] = {};
-    char *sp = serial;
-    for (int i=0; i<48; i+=3) {
-      *sp = spsser[i];
-      sp++;
-      *sp = spsser[i+1];
-      sp++;
-    }
-    ESP_LOGI("SPS30", "Serial '%s'", serial);
+  // Read serial
+  char spsser[32] = {};
+  err = sensor_send(sps30, "\xd0\x33", 2, 1, spsser, 48);
+  if (!err) {
+    ESP_LOGI("SPS30", "Serial '%s'", spsser);
   }
 
   // Start measurements, report floats
-  err = Core2ForAWS_Port_A_I2C_Write(sps30, I2C_NO_REG, (uint8_t*) "\x00\x10\x03\x00\xac", 5);
-  vTaskDelay(pdMS_TO_TICKS(20));
+  sensor_send(sps30, "\x00\x10\x03\x00\xac", 5, 20, 0, 0);
 
 
-  I2CDevice_t scd30 = Core2ForAWS_Port_A_I2C_Begin(0x61, PORT_A_I2C_STANDARD_BAUD);
+  struct sensor *scd30 = init_sensor(0x61, "SCD30");
   // Reset
-  //err = Core2ForAWS_Port_A_I2C_Write(scd30, I2C_NO_REG, (uint8_t*) "\xd3\x04", 2);
-  //vTaskDelay(pdMS_TO_TICKS(5));
+  //sensor_send(scd30, "\xd3\x04", 2, 5, 0, 0);
 
   // Get FW version
-  err = Core2ForAWS_Port_A_I2C_Write(scd30, I2C_NO_REG, (uint8_t*) "\xd1\x00", 2);
-  vTaskDelay(pdMS_TO_TICKS(1));
-  uint8_t scdver[3] = {};
-  err = Core2ForAWS_Port_A_I2C_Read(scd30, I2C_NO_REG, scdver, sizeof(scdver));
-  if(!err){
+  uint8_t scdver[2] = {};
+  err = sensor_send(scd30, "\xd1\x00", 2, 1, scdver, 3);
+  if (!err) {
     ESP_LOGI("SCD30", "FW ver %d.%d", scdver[0], scdver[1]);
   }
 
-  err = Core2ForAWS_Port_A_I2C_Write(scd30, I2C_NO_REG, (uint8_t*) "\x00\x10\x00\x00\x81", 5);
-  vTaskDelay(pdMS_TO_TICKS(5));
+  // Start measurements
+  sensor_send(scd30, "\x00\x10\x00\x00\x81", 5, 5, 0, 0);
 
 
-  I2CDevice_t gm = Core2ForAWS_Port_A_I2C_Begin(0x08, PORT_A_I2C_STANDARD_BAUD);
+  struct sensor *gm = init_sensor(0x08, "GMx02B");
   // Warm up sensors
-  err = Core2ForAWS_Port_A_I2C_Write(gm, I2C_NO_REG, (uint8_t*) "\xFE", 1);
+  sensor_send(gm, "\xfe", 1, 0, 0, 0);
 
 
   vTaskDelay(pdMS_TO_TICKS(50));
@@ -125,35 +156,28 @@ void i2c_task(void *par) {
   float pms[10] = {};
   float coo[3] = {};
   uint32_t no2 = 0, alc = 0, voc = 0, co = 0;
-
+  uint16_t vocs = 0;
 
   for(;;){
+    // Start measurements once per second
+    struct timeval now;
+    gettimeofday(&now, 0);
+    vTaskDelay(pdMS_TO_TICKS(1000-now.tv_usec/1000));
+
 
     // Read temp & RH
-    //err = Core2ForAWS_Port_A_I2C_Write(sht40, I2C_NO_REG, (uint8_t*) "\xfd", 1);
-    //err = Core2ForAWS_Port_A_I2C_Write(sht40, I2C_NO_REG, (uint8_t*) "\xf6", 1);
-    err = Core2ForAWS_Port_A_I2C_Write(sht40, I2C_NO_REG, (uint8_t*) "\xe0", 1);
-    vTaskDelay(pdMS_TO_TICKS(10));
-
-    uint8_t tmp[6] = {};
-    err = Core2ForAWS_Port_A_I2C_Read(sht40, I2C_NO_REG, tmp, sizeof(tmp));
-    if(!err){
-      if (SensCrc(&tmp[0]) != tmp[2] || SensCrc(&tmp[3]) != tmp[5]) {
-        ESP_LOGI("SHT40", "Sensor CRC fail");
-      }
-      temp = -45 + 175.0*(tmp[0]*256 + tmp[1])/65535;
-      rh = -6 + 125.0*(tmp[3]*256 + tmp[4])/65535;
+    uint16_t temps[2] = {};
+    // Precision: e0, f6, fd (highest)
+    err = sensor_send(sht40, "\xe0", 1, 10, temps, 6);
+    if (!err) {
+      temp = -45 + 175.0*ntohs(temps[0])/65535;
+      rh = -6 + 125.0*ntohs(temps[1])/65535;
       ESP_LOGI("SHT40", "Temp %.2f, RH %.2f", temp, rh);
-    } else {
-      ESP_LOGI(TAG, "Read temp? %s", esp_err_to_name(err));
     }
 
-    vTaskDelay(pdMS_TO_TICKS(20));
-
-    //int vocs = 0;
     /*
     // Read VOC
-    uint8_t sgpmeas[6] = { 0x26, 0x0f };
+    uint8_t sgpmeas[8] = { 0x26, 0x0f };
     int x;
     x = rh * 65535 / 100;
     sgpmeas[2] = x>>8;
@@ -164,82 +188,51 @@ void i2c_task(void *par) {
     sgpmeas[6] = x & 0xFF;
     sgpmeas[7] = SensCrc(&sgpmeas[5]);
 
-    err = Core2ForAWS_Port_A_I2C_Write(sgp40, I2C_NO_REG, sgpmeas, sizeof(sgpmeas));
-    vTaskDelay(pdMS_TO_TICKS(30));
-
-    uint8_t voc[3] = {};
-    err = Core2ForAWS_Port_A_I2C_Read(sgp40, I2C_NO_REG, tmp, sizeof(tmp));
+    uint16_t voctmp = 0;;
+    err = sensor_send(sgp40, sgpmeas, 8, 30, voc, 3);
     if(!err){
-      if (SensCrc(&voc[0]) != voc[2]) {
-        ESP_LOGI("SGP40", "Sensor CRC fail");
-      }
-      vocs = (voc[0]<<8) + voc[1];
+      vocs = ntohs(voctmp);
       ESP_LOGI("SGP40", "Voc %d", vocs);
     }
     */
 
-    vTaskDelay(pdMS_TO_TICKS(20));
-
     // Read PM2.5
-    err = Core2ForAWS_Port_A_I2C_Write(sps30, I2C_NO_REG, (uint8_t*) "\x03\x00", 2);
-    vTaskDelay(pdMS_TO_TICKS(1));
-
-    uint8_t atmp[60] = {};
-    err = Core2ForAWS_Port_A_I2C_Read(sps30, I2C_NO_REG, atmp, sizeof(atmp));
+    uint32_t atmp[10] = {};
+    err = sensor_send(sps30, "\x03\x00", 2, 1, atmp, 60);
     if (!err) {
-      for (int i = 0; i<60; i+=6) {
-        // After every 2 bytes the checksum of previous two bytes is transferred
-        uint8_t vt[4];
-        vt[0] = atmp[i+4];
-        vt[1] = atmp[i+3];
-        vt[2] = atmp[i+1];
-        vt[3] = atmp[i+0];
-
-        pms[i/6] = *(float*)vt;
+      // Reverse byte order and cast to floats
+      for (int i=0; i<10; i++) {
+        atmp[i] = ntohl(atmp[i]);
+        pms[i] = *(float*)&atmp[i];
       }
-      ESP_LOGI("SPS30", "PMS: %.2f %.2f %.2f %.2f %.2f", pms[0], pms[1], pms[2], pms[3], pms[4]);
+      ESP_LOGI("SPS30", "Mass: %.2f %.2f %.2f %.2f, Num: %.2f %.2f %.2f %.2f %.2f, Size: %.2f", pms[0], pms[1], pms[2], pms[3], pms[4], pms[5], pms[6], pms[7], pms[8], pms[9]);
     }
 
-    vTaskDelay(pdMS_TO_TICKS(20));
 
     // Read CO2 status
-    err = Core2ForAWS_Port_A_I2C_Write(scd30, I2C_NO_REG, (uint8_t*) "\x02\x02", 2);
-    vTaskDelay(pdMS_TO_TICKS(3));
-    uint8_t st[2] = {};
-    err = Core2ForAWS_Port_A_I2C_Read(scd30, I2C_NO_REG, st, sizeof(st));
-    if (!err) {
-      ESP_LOGI("SCD30", "Data ready: %d %d", st[0], st[1]);
+    uint16_t scdstatus = 0;
+    err = sensor_send(scd30, "\x02\x02", 2, 3, &scdstatus, 3);
+    if (!err && scdstatus) {
+      uint32_t cotmp[3] = {};
+      err = sensor_send(scd30, "\x03\x00", 2, 5, cotmp, 18);
 
-      if (st[0] || st[1]) {
-        err = Core2ForAWS_Port_A_I2C_Write(scd30, I2C_NO_REG, (uint8_t*) "\x03\x00", 2);
-        vTaskDelay(pdMS_TO_TICKS(5));
-        uint8_t coval[18] = {};
-        err = Core2ForAWS_Port_A_I2C_Read(scd30, I2C_NO_REG, coval, sizeof(coval));
-        if (!err) {
-          for (int i = 0; i<18; i+=6) {
-            // After every 2 bytes the checksum of previous two bytes is transferred
-            uint8_t vt[4];
-            vt[0] = coval[i+4];
-            vt[1] = coval[i+3];
-            vt[2] = coval[i+1];
-            vt[3] = coval[i+0];
-            coo[i/6] = *(float*)vt;
-          }
-
-          ESP_LOGI("SCD30", "CO2 %.2f, T %.2f, RH %.2f", coo[0], coo[1], coo[2]);
+      if (!err) {
+        // Reverse byte order and cast to floats
+        for (int i=0; i<10; i++) {
+          cotmp[i] = ntohl(cotmp[i]);
+          coo[i] = *(float*)&cotmp[i];
         }
+
+        ESP_LOGI("SCD30", "CO2 %.2f, T %.2f, RH %.2f", coo[0], coo[1], coo[2]);
       }
     }
 
     // Gas sensors
-    err = Core2ForAWS_Port_A_I2C_Write(gm, I2C_NO_REG, (uint8_t*) "\x01", 2);
-    err = Core2ForAWS_Port_A_I2C_Read(gm, I2C_NO_REG, &no2, sizeof(no2));
-    err = Core2ForAWS_Port_A_I2C_Write(gm, I2C_NO_REG, (uint8_t*) "\x03", 2);
-    err = Core2ForAWS_Port_A_I2C_Read(gm, I2C_NO_REG, &alc, sizeof(alc));
-    err = Core2ForAWS_Port_A_I2C_Write(gm, I2C_NO_REG, (uint8_t*) "\x05", 2);
-    err = Core2ForAWS_Port_A_I2C_Read(gm, I2C_NO_REG, &voc, sizeof(voc));
-    err = Core2ForAWS_Port_A_I2C_Write(gm, I2C_NO_REG, (uint8_t*) "\x07", 2);
-    err = Core2ForAWS_Port_A_I2C_Read(gm, I2C_NO_REG, &co, sizeof(co));
+
+    err = sensor_send(gm, "\x01", 1, 0, &no2, sizeof(no2));
+    err = sensor_send(gm, "\x03", 1, 0, &alc, sizeof(alc));
+    err = sensor_send(gm, "\x05", 1, 0, &voc, sizeof(voc));
+    err = sensor_send(gm, "\x07", 1, 0, &co, sizeof(co));
     ESP_LOGI("GMx02B", "NO2 %u, Alc %u, VOC %u, CO %u", no2, alc, voc, co);
 
 
@@ -249,8 +242,6 @@ void i2c_task(void *par) {
         temp, coo[1], rh, coo[2], coo[0], pms[0], pms[1], pms[2], pms[3], pms[9],
         no2*3.3/1023, alc*3.3/1023, voc*3.3/1023, co*3.3/1023);
     xSemaphoreGive(xGuiSemaphore);
-
-    vTaskDelay(pdMS_TO_TICKS(1000));
   }
 
   vTaskDelete(NULL);
